c39d45639543215f826e7026d2f4372e149687e8
GoDGamEz <chisanuchabest@hotmail.com>
2024-10-20 01:22:24 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -479,7 +479,7 @@ const App = () => {
                         placeholder="ชื่อนักเดินทาง"
                         value={userName}
                         onChange={(e) => setUserName(e.target.value)}
-                        className="lg:p-[0.4vw] p-[0.54vw] border border-gray-300 outline-none lg:rounded-[0.6vw] rounded-[0.8vw] lg:mb-[0.8vw] mb-[1.2vw] xl:text-[1vw] lg:text-[1.2vw] md:text-[1.4vw] text-[1.6vw]"
+                        className="lg:p-[0.4vw] p-[0.5vw] border border-gray-300 outline-none lg:rounded-[0.6vw] rounded-[0.8vw] lg:mb-[0.8vw] mb-[1.2vw] xl:text-[1vw] lg:text-[1.2vw] md:text-[1.4vw] text-[1.6vw]"
                         required
                       />
                       <button
